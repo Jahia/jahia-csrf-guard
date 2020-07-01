5587cecc9cba75204d73441bb4ba91d06e6f9a1b
dgaillard <dgaillard@jahia.com>
2020-07-01 17:04:28 +0200

    BACKLOG-13986 add filter to add Javascript tag on all pages of the product, the configuration of the filter need to be refined

---
+++ src/main/java/org/jahia/modules/jahiacsrfguard/filters/CsrfGuarfJSFilter.java
@@ -0,0 +1,31 @@
+package org.jahia.modules.jahiacsrfguard.filters;
+
+import net.htmlparser.jericho.*;
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.Resource;
+import org.jahia.services.render.filter.AbstractFilter;
+import org.jahia.services.render.filter.RenderChain;
+
+import java.util.List;
+
+public class CsrfGuarfJSFilter extends AbstractFilter {
+    @Override
+    public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
+        String output = super.execute(previousOut, renderContext, resource, chain);
+        String jsTagToAdd = "<script src=\"/" + renderContext.getURLGenerator().getContext() + "/JavaScripServlet\"></script>";
+        output = addJsTag(output, jsTagToAdd);
+        return output;
+    }
+
+    private String addJsTag(String output, String jsTagToAdd) {
+        Source source = new Source(output);
+        OutputDocument outputDocument = new OutputDocument(source);
+        List<Element> elementList = source.getAllElements(HTMLElementName.HEAD);
+        if (elementList != null && !elementList.isEmpty()) {
+            final EndTag bodyEndTag = elementList.get(0).getEndTag();
+            outputDocument.replace(bodyEndTag.getBegin(), bodyEndTag.getBegin() + 1, jsTagToAdd);
+        }
+        output = outputDocument.toString().trim();
+        return output;
+    }
+}
