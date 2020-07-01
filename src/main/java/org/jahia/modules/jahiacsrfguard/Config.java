28f52d8239b072d1816084188dcf8b72b7fbe476
Thomas Draier <tdraier@jahia.com>
2020-07-01 15:16:38 +0200

    BACKLOG-14002: Added config and whitelist (#11)

---
+++ src/main/java/org/jahia/modules/jahiacsrfguard/Config.java
@@ -0,0 +1,66 @@
+package org.jahia.modules.jahiacsrfguard;
+
+import org.jahia.modules.jahiacsrfguard.filters.CsrfGuardFilterWrapper;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Config {
+
+    private CsrfGuardFilterWrapper filter;
+
+    private List<Pattern> urlPatterns;
+    private List<Pattern> whitelist;
+
+    public void setFilter(CsrfGuardFilterWrapper filter) {
+        this.filter = filter;
+    }
+
+    public void init() {
+        filter.registerConfig(this);
+    }
+
+    public void destroy() {
+        filter.unregisterConfig(this);
+    }
+
+    public void setUrlPatterns(String urlPatterns) {
+        this.urlPatterns = Arrays.stream(urlPatterns.split(",")).map(String::trim).map(Config::createUrlPattern).collect(Collectors.toList());
+    }
+
+    public void setWhitelist(String whitelist) {
+        this.whitelist = Arrays.stream(whitelist.split(",")).map(String::trim).map(Config::createUrlPattern).collect(Collectors.toList());
+    }
+
+    public static Pattern createUrlPattern(String pattern) {
+        String patternToUse = pattern;
+        if (!pattern.contains("*")) {
+            patternToUse = pattern + (pattern.endsWith("/") ? "*" : "/*");
+        }
+        patternToUse = patternToUse.replace(".", "\\.");
+        patternToUse = patternToUse.replace("*", ".*");
+        return Pattern.compile(patternToUse);
+    }
+
+    public boolean isFiltered(ServletRequest request) {
+        if (urlPatterns == null) {
+            return false;
+        }
+
+        String uri = ((HttpServletRequest) request).getRequestURI();
+        return urlPatterns.stream().anyMatch(pattern -> pattern.matcher(uri).matches());
+    }
+
+    public boolean isWhiteListed(ServletRequest request) {
+        if (whitelist == null) {
+            return false;
+        }
+        String uri = ((HttpServletRequest) request).getRequestURI();
+        return whitelist.stream().anyMatch(pattern -> pattern.matcher(uri).matches());
+    }
+
+}
--- src/main/java/org/jahia/modules/jahiacsrfguard/filters/CsrfGuardFilterWrapper.java
+++ src/main/java/org/jahia/modules/jahiacsrfguard/filters/CsrfGuardFilterWrapper.java
@@ -1,13 +1,17 @@
 package org.jahia.modules.jahiacsrfguard.filters;
 
 import org.jahia.bin.filters.AbstractServletFilter;
+import org.jahia.modules.jahiacsrfguard.Config;
 import org.owasp.csrfguard.CsrfGuardFilter;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CsrfGuardFilterWrapper extends AbstractServletFilter {
     private CsrfGuardFilter csrfGuardFilter;
+    private List<Config> configs = new ArrayList<>();
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -17,11 +21,32 @@ public class CsrfGuardFilterWrapper extends AbstractServletFilter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        csrfGuardFilter.doFilter(request, response, chain);
+        if (isFiltered(request) && !isWhiteListed(request)) {
+            csrfGuardFilter.doFilter(request, response, chain);
+            return;
+        }
+
+        chain.doFilter(request, response);
     }
 
     @Override
     public void destroy() {
         csrfGuardFilter.destroy();
+    }
+
+    public void registerConfig(Config config) {
+        configs.add(config);
+    }
+
+    public void unregisterConfig(Config config) {
+        configs.remove(config);
+    }
+
+    public boolean isFiltered(ServletRequest request) {
+        return configs.stream().anyMatch(config -> config.isFiltered(request));
+    }
+
+    public boolean isWhiteListed(ServletRequest request) {
+        return configs.stream().anyMatch(config -> config.isWhiteListed(request));
     }
 }
