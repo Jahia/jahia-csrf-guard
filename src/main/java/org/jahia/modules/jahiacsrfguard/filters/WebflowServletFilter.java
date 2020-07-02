29b14b8f81935e5a0694c0a5573c144c70a7817a
dgaillard <dgaillard@jahia.com>
2020-07-02 18:32:29 +0200

    BACKLOG-14038 filter webflow request to remove the OWASP token since it's breaking webflow

---
+++ src/main/java/org/jahia/modules/jahiacsrfguard/filters/WebflowServletFilter.java
@@ -0,0 +1,69 @@
+package org.jahia.modules.jahiacsrfguard.filters;
+
+import org.jahia.bin.filters.AbstractServletFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+public class WebflowServletFilter extends AbstractServletFilter {
+
+    private static final String OWASP_CSRFTOKEN = "OWASP-CSRFTOKEN";
+    private static final String WEBFLOW_TOKEN = "webflowToken";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Nothing to do
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request.getParameterMap().containsKey(WEBFLOW_TOKEN) && request.getParameterMap().containsKey(OWASP_CSRFTOKEN)) {
+            HttpServletRequest req = new WebflowServletRequestWrapper((HttpServletRequest) request, new HashMap<>(request.getParameterMap()));
+            chain.doFilter(req, response);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // Nothing to do
+    }
+
+    private static class WebflowServletRequestWrapper extends HttpServletRequestWrapper {
+        private final Map<String, String[]> parameterMap;
+
+        public WebflowServletRequestWrapper(HttpServletRequest request, Map<String, String[]> parameterMap) {
+            super(request);
+            parameterMap.remove(OWASP_CSRFTOKEN);
+            this.parameterMap = parameterMap;
+        }
+
+        @Override
+        public String getParameter(String name) {
+            return (parameterMap.get(name) != null && parameterMap.get(name).length > 0) ? parameterMap.get(name)[0] : null;
+        }
+
+        @Override
+        public Map<String, String[]> getParameterMap() {
+            return parameterMap;
+        }
+
+        @Override
+        public Enumeration<String> getParameterNames() {
+            return new Vector<>(parameterMap.keySet()).elements();
+        }
+
+        @Override
+        public String[] getParameterValues(String name) {
+            return parameterMap.get(name);
+        }
+    }
+}
+
