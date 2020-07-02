ce0c7d026fdad695e97458528054ccc7112e8f05
dgaillard <dgaillard@jahia.com>
2020-07-02 13:04:46 +0200

    BACKLOG-13986 code review changes

--- src/main/java/org/jahia/modules/jahiacsrfguard/filters/CsrfGuarfJSFilter.java
+++ src/main/java/org/jahia/modules/jahiacsrfguard/filters/CsrfGuarfJSFilter.java
@@ -1,32 +1,22 @@
 package org.jahia.modules.jahiacsrfguard.filters;
 
-import net.htmlparser.jericho.*;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.filter.AbstractFilter;
 import org.jahia.services.render.filter.RenderChain;
 
-import java.util.List;
-
+/**
+ * The CSRF Guard Filter add the csrfguard JS tag to the page,
+ * this JS add the CSRF token to the required elements (e.g: form, ajax request, ...)
+ */
 public class CsrfGuarfJSFilter extends AbstractFilter {
 
     @Override
     public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
         String output = super.execute(previousOut, renderContext, resource, chain);
-        String jsTagToAdd = "\n<jahia:resource type=\"javascript\" path=\"" + renderContext.getURLGenerator().getContext() + "/JavaScriptServlet\"></jahia:resource>\n<";
-        output = addJsTag(output, jsTagToAdd);
-        return output;
-    }
-
-    private String addJsTag(String output, String jsTagToAdd) {
-        Source source = new Source(output);
-        OutputDocument outputDocument = new OutputDocument(source);
-        List<Element> elementList = source.getAllElements(HTMLElementName.HEAD);
-        if (elementList != null && !elementList.isEmpty()) {
-            final EndTag bodyEndTag = elementList.get(0).getEndTag();
-            outputDocument.replace(bodyEndTag.getBegin(), bodyEndTag.getBegin() + 1, jsTagToAdd);
-        }
-        output = outputDocument.toString().trim();
+        String jsTagToAdd = "\n<jahia:resource type=\"javascript\" path=\""
+                + renderContext.getURLGenerator().getContext() + "/JavaScriptServlet\"></jahia:resource>\n<";
+        output += jsTagToAdd;
         return output;
     }
 }
