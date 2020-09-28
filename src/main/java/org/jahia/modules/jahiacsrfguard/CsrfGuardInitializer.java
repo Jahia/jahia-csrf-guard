20f5731e3c0800ae642e092b199ab2ee1328b9d9
Thomas Draier <tdraier@jahia.com>
2020-09-28 10:31:30 +0200

    QA-13186: Alternative config for ibm jdk (#29)

--- src/main/java/org/jahia/modules/jahiacsrfguard/CsrfGuardInitializer.java
+++ src/main/java/org/jahia/modules/jahiacsrfguard/CsrfGuardInitializer.java
@@ -3,11 +3,28 @@ package org.jahia.modules.jahiacsrfguard;
 import org.jahia.bin.listeners.JahiaContextLoaderListener;
 import org.owasp.csrfguard.CsrfGuardServletContextListener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 public class CsrfGuardInitializer {
     public void onStart() {
         CsrfGuardServletContextListener csrfGuardServletContextListener = new CsrfGuardServletContextListener();
-        csrfGuardServletContextListener.contextInitialized(new ServletContextEvent(JahiaContextLoaderListener.getServletContext()));
+
+        ServletContext context = JahiaContextLoaderListener.getServletContext();
+
+        context = (ServletContext) Proxy.newProxyInstance(CsrfGuardInitializer.class.getClassLoader(), new Class[]{ServletContext.class}, (Object proxy, Method method, Object[] args) -> {
+            if (method.getName().equals("getInitParameter") && args[0].equals("Owasp.CsrfGuard.Config")) {
+                if (System.getProperty("java.vm.vendor").toLowerCase().contains("ibm")) {
+                    return "META-INF/csrfguard-ibm.properties";
+                }
+                return "META-INF/csrfguard.properties";
+            } else {
+                return method.invoke(JahiaContextLoaderListener.getServletContext(), args);
+            }
+        });
+
+        csrfGuardServletContextListener.contextInitialized(new ServletContextEvent(context));
     }
 }
