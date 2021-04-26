376f3b910f3852a554ab22a503dc81c0398f2a96
Florent BOURASSE <fbourasse@jahia.com>
2021-04-26 14:12:44 +0200

    QA-13722: definition of endsWith for IE11

--- src/main/resources/META-INF/csrfguard.template.js
+++ src/main/resources/META-INF/csrfguard.template.js
@@ -407,6 +407,18 @@
     }
 
     function isDotDoUrl(url) {
+        if (!String.prototype.endsWith) {
+            String.prototype.endsWith = function(searchString, position) {
+                var subjectString = this.toString();
+                if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+                    position = subjectString.length;
+                }
+                position -= searchString.length;
+                var lastIndex = subjectString.lastIndexOf(searchString, position);
+                return lastIndex !== -1 && lastIndex === position;
+            };
+        }		
+		
         let pathPart = (url.indexOf('?') !== -1) ? url.substring(0, url.indexOf('?')) : url;
         return pathPart.endsWith('.do') || pathPart.indexOf('/*') > -1;
     }
