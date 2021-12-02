f8da0d293051164764f74d87fae90f0e40053ba9
Benjamin Papez <bpapez@jahia.com>
2021-12-02 14:31:45 +0100

    QA-13942: Refactor providing configuration and registering servlet (#41)
    * QA-13942: Refactor providing configuration and registering servlet
    
    - bind/Unbind CsrfGuard servlet via HttpSAerviceListener
    - override the way configuraion is read
    -- first csrfguard-3.1.0.properties (taken from csrfguard-3.1.0.jar)
    -- then csrfguard-jahia.properties
    -- then optionally csrfguard-ibm.properties (on WebSphere)
    -- then /karaf/etc/org.owasp.csrfguard.cfg (customers can set their own
    configuration here)
    
    * QA-13492: Update circleci to latest configuration
    
    * QA-13942: Fix sonarqube defects
    
    * QA-13942: remove integration-test steps as there are none yet
    
    * QA-13942: Copy .github from sandbox
    
    * QA-13942: Remove no longer needed github workflow file
    
    Conflicts:
    	pom.xml

---
+++ src/main/java/org/jahia/modules/jahiacsrfguard/HttpServiceListener.java
@@ -0,0 +1,91 @@
+/*
+ * ==========================================================================================
+ * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
+ * ==========================================================================================
+ *
+ *                                  http://www.jahia.com
+ *
+ * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2021 Jahia Solutions Group. All rights reserved.
+ *
+ *     This file is part of a Jahia's Enterprise Distribution.
+ *
+ *     Jahia's Enterprise Distributions must be used in accordance with the terms
+ *     contained in the Jahia Solutions Group Terms & Conditions as well as
+ *     the Jahia Sustainable Enterprise License (JSEL).
+ *
+ *     For questions regarding licensing, support, production usage...
+ *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
+ *
+ * ==========================================================================================
+ */
+package org.jahia.modules.jahiacsrfguard;
+
+import org.eclipse.gemini.blueprint.context.BundleContextAware;
+import org.jahia.bin.listeners.JahiaContextLoaderListener;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.owasp.csrfguard.CsrfGuardServletContextListener;
+import org.owasp.csrfguard.servlet.JavaScriptServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletException;
+
+/**
+ * Service listener to make sure that we don't block startup
+ */
+public class HttpServiceListener implements BundleContextAware {
+
+    public static final Logger logger = LoggerFactory.getLogger(HttpServiceListener.class);
+
+    JavaScriptServlet javaScriptServlet;
+    BundleContext bundleContext;
+
+    public void setBundleContext(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+    public void setJavaScriptServlet(JavaScriptServlet javaScriptServlet) {
+        this.javaScriptServlet = javaScriptServlet;
+    }
+    
+    /**
+     * @param serviceReference The passed service reference, but it is a proxy class that we cannot use to retrieve the
+                               real service object, so we simply look it up again
+     */
+    public void onBind(ServiceReference serviceReference) {
+        ServiceReference realServiceReference = bundleContext.getServiceReference(HttpService.class.getName());
+        HttpService httpService = (HttpService) bundleContext.getService(realServiceReference);
+        try {
+            httpService.registerServlet("/CsrfServlet", javaScriptServlet, null, null);
+            logger.info("Successfully registered custom servlet at /modules/CsrfServlet");
+            
+            CsrfGuardServletContextListener csrfGuardServletContextListener = new CsrfGuardServletContextListener();
+            csrfGuardServletContextListener.contextInitialized(new ServletContextEvent(JahiaContextLoaderListener.getServletContext()));
+        } catch (ServletException | NamespaceException e) {
+            logger.error("Error registering servlet", e);
+        }
+    }
+
+    public void onUnbind(ServiceReference serviceReference) {
+        if (serviceReference == null) {
+            return;
+        }
+        ServiceReference realServiceReference = bundleContext.getServiceReference(HttpService.class.getName());
+        if (realServiceReference == null) {
+            return;
+        }
+        HttpService httpService = (HttpService) bundleContext.getService(realServiceReference);
+        if (httpService == null) {
+            return;
+        }
+        httpService.unregister("/CsrfServlet");
+        logger.info("Successfully unregistered custom servlet from /modules/CsrfServlet");
+    }
+}
