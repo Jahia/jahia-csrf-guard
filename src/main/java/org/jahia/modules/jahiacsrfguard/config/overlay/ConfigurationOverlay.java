208c98f237930b972d80fc4ce63c17dcbb6f8b8d
Benjamin Papez <bpapez@jahia.com>
2021-12-17 14:52:52 +0100

    BACKLOG-15401: Backport stability to 1_x (unrelated to TokenPerPage) (#49)

--- src/main/java/org/jahia/modules/jahiacsrfguard/config/overlay/ConfigurationOverlay.java
+++ src/main/java/org/jahia/modules/jahiacsrfguard/config/overlay/ConfigurationOverlay.java
@@ -1,9 +1,32 @@
+/*
+ * ==========================================================================================
+ * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
+ * ==========================================================================================
+ *
+ *                                  http://www.jahia.com
+ *
+ * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2021 Jahia Solutions Group. All rights reserved.
+ *
+ *     This file is part of a Jahia's Enterprise Distribution.
+ *
+ *     Jahia's Enterprise Distributions must be used in accordance with the terms
+ *     contained in the Jahia Solutions Group Terms & Conditions as well as
+ *     the Jahia Sustainable Enterprise License (JSEL).
+ *
+ *     For questions regarding licensing, support, production usage...
+ *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
+ *
+ * ==========================================================================================
+ */
 package org.jahia.modules.jahiacsrfguard.config.overlay;
 
 import java.util.Dictionary;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedService;
@@ -23,7 +46,7 @@ public class ConfigurationOverlay implements ManagedService {
             overlayProperties = null;
             return;
         }
-        Map<String, String> csrfGuardProperties = new HashMap<>();
+        Map<String, String> csrfGuardProperties = new ConcurrentHashMap<>();
         Enumeration<String> keys = properties.keys();
         while (keys.hasMoreElements()) {
             String key = keys.nextElement();
